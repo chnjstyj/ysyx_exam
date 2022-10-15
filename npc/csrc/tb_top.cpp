a19a081703161f8b084b82303b506b391ae75bf0
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-15 16:12:11 +0800

    > sim RTL 22050577 Tang Yingjie Linux tang-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:12:11 up  6:03,  1 user,  load average: 0.90, 0.76, 0.56

--- npc/csrc/tb_top.cpp
+++ npc/csrc/tb_top.cpp
@@ -5,7 +5,9 @@
 #include <verilated_vcd_c.h>
 #include <nvboard.h>
 
-#include "Vtop.h"
+#include "Vtop.h" 
+
+static Vtop top;
 
 #define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
@@ -14,13 +16,12 @@ void nvboard_bind_all_pins(Vtop* top);
 
 int main()
 {
-    Vtop *top = new Vtop;
-    nvboard_bind_all_pins(top);
+    //Vtop *top = new Vtop;
+    nvboard_bind_all_pins(&top);
     nvboard_init();
     while(1)
     {
         nvboard_update();
     }
-    delete top;
     return 0;
 }
