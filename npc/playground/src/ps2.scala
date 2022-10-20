bbb18bdbc88f6725428f47fa3965514590089ebe
22050577-Tang Yingjie <chnjstyj@outlook.com>
2022-10-20 22:27:48 +0800

    exp9

--- npc/playground/src/ps2.scala
+++ npc/playground/src/ps2.scala
@@ -9,6 +9,8 @@ class ps2 extends Module {
     val ps2_clk = Input(UInt(1.W))
     val ps2_data = Input(UInt(1.W))
     val data = Output(UInt(8.W))
+    val valid = Output(UInt(1.W))
+    val ready = Output(UInt(1.W))
   })
 
   val ps2_clk_sync = Reg(UInt(3.W))
@@ -26,12 +28,16 @@ class ps2 extends Module {
 
   val break = RegInit(0.U(1.W))
 
+  val ready = RegInit(0.U(1.W))
+  io.ready := ready
+
   val mem = Mem(255,UInt(8.W)) 
   loadMemoryFromFile(mem,"keyboard.hex")
   
   ps2_clk_sync := Cat(ps2_clk_sync(1,0),io.ps2_clk)
 
   io.data := data_reg
+  io.valid := (~break) & en
 
   when(sampling === 1.U){
     when(count === 10.U){
@@ -42,6 +48,7 @@ class ps2 extends Module {
           break := 1.U
         }.elsewhen(buffer.asUInt(8,1) =/= 0.U){
           //data_reg := convert(buffer.asUInt(8,1))
+          ready := 1.U
           data_reg := mem.read(buffer.asUInt(8,1))
           when(break === 1.U){
             en := 0.U
@@ -51,12 +58,14 @@ class ps2 extends Module {
           }
         }
       }
-    count := 0.U
+      count := 0.U
     }.otherwise{    
       buffer(count) := io.ps2_data
       count := count + 1.U
+      ready := 0.U
     }
   }.otherwise{
+    ready := 0.U
   }
   
   //io.data := fifo(r_ptr + 1.U)
--- npc/playground/src/top.scala
+++ npc/playground/src/top.scala
@@ -6,6 +6,8 @@ import chisel3.util.experimental.loadMemoryFromFileInline
 
 class top extends Module{
     val io = IO(new Bundle{
+        val ps2_clk = Input(UInt(1.W))
+        val ps2_data = Input(UInt(1.W))
         val VGA_HSYNC = Output(UInt(1.W))
         val VGA_VSYNC = Output(UInt(1.W))
         val VGA_BLANK_N = Output(UInt(1.W))
@@ -18,6 +20,7 @@ class top extends Module{
     })
 
     val vga_ctrl = Module(new vga_ctrl)
+    val ps2 = Module(new ps2)
 
     val vga_data = Wire(UInt(24.W))
     val h_addr = Wire(UInt(10.W))
@@ -29,18 +32,59 @@ class top extends Module{
     val v_count = RegInit(0.U(8.W))
     val h_count = RegInit(0.U(8.W))
 
+    /*
     val mem = Mem(307200,UInt(24.W)) 
     val addr = RegInit(0.U(64.W))
-    loadMemoryFromFile(mem,"pic.hex")
+    loadMemoryFromFile(mem,"pic.hex")*/
+
     val font_mem = Mem(4096,UInt(12.W))
     loadMemoryFromFile(font_mem,"vga_font.txt")
     val font_vec = Mem(2176,UInt(8.W))
     val font_addr = WireDefault(v_count * 71.U + h_count)
     io.font_addr := font_addr
 
-    for (i <- 0 until 100)
+    val write_ptr = RegInit(0.U(12.W))
+    val writed_lines = RegInit(0.U(8.W))
+
+    //val ps2_data_ready
+
+    //initial
+    /*
+    for (i <- 0 until 2176)
     {
-        font_vec(i) := "h41".U
+        font_vec(i) := "h00".U
+    }*/
+
+    when((ps2.io.valid & ps2.io.ready) === 1.U){
+        //enter
+        when(ps2.io.data === "h0a".U ){
+            //end of line
+            when(write_ptr - 71.U * writed_lines === 0.U){
+                //write_ptr := write_ptr + 1.U
+                //writed_lines := writed_lines + 1.U
+            }.otherwise{
+                write_ptr := write_ptr + (71.U - (write_ptr - (writed_lines - 1.U) * 71.U))
+                font_vec(write_ptr) := 0.U
+                writed_lines := writed_lines + 1.U
+            }
+        //backspace
+        }.elsewhen(ps2.io.data === "h08".U){
+            font_vec(write_ptr) := "h20".U
+            when (write_ptr =/= 0.U)
+            {
+                write_ptr := write_ptr - 1.U
+            }
+        } .otherwise{
+            when(ps2.io.data =/= 0.U){
+                write_ptr := write_ptr + 1.U
+                font_vec(write_ptr) := ps2.io.data
+                when(write_ptr - 71.U * writed_lines === 0.U){
+                    writed_lines := writed_lines + 1.U
+                }
+            }
+        }
+    }.otherwise{
+        font_vec(write_ptr) := 0.U
     }
 
     when(vga_ctrl.io.valid === 1.U){
@@ -87,6 +131,7 @@ class top extends Module{
         vga_data := 0.U(24.W)
     }
 
+    /*
     when(vga_ctrl.io.valid === 1.U){
         addr := addr + 1.U
         when(h_addr === 639.U && v_addr === 479.U){
@@ -94,7 +139,7 @@ class top extends Module{
         }
     }.otherwise{
         //addr := 0.U
-    }
+    }*/
 
     vga_ctrl.io.vga_data := vga_data
     h_addr := vga_ctrl.io.h_addr
@@ -105,6 +150,9 @@ class top extends Module{
     io.VGA_R := vga_ctrl.io.vga_r
     io.VGA_G := vga_ctrl.io.vga_g
     io.VGA_B := vga_ctrl.io.vga_b
+
+    ps2.io.ps2_clk := io.ps2_clk
+    ps2.io.ps2_data := io.ps2_data
     
     io.h_count := h_count
     io.v_count := v_count
